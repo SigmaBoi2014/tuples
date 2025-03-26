3867580f60c7e0c180f1f37746aea397f2797987
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-03-26 08:59:28 -0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,14 @@
+turtle=[1,2,3],2,"z",9,8,"o"
+print(turtle)
+print(type(turtle))
+address=(227,"Brickfield Spooder","Hello",6354782)
+for i in address:
+    print(i)
+house_number,name,message,pin=address
+print(house_number,name,message,pin)
+print(turtle[1])
+print(turtle[0][2])
+print(turtle[1:])
+print(turtle[2:5])
+turtle=(1,5,4,3,2)
+print(turtle)
