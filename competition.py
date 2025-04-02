ed3122486814d38b868206eee83b546d33b144ba
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-04-02 07:26:59 -0700

    Add files via upload

---
+++ competition.py
@@ -0,0 +1,17 @@
+group1=("SwimmingSpooders","Jan.5,2079","cool school","SwimmingMedal")
+name,date,venue,medalType=group1
+print(group1)
+print("name",name+"\n"+"date",date+"\n"+"venue",venue+"\n"+"medalType",medalType+"\n")
+"""print(type(group1))
+group2=size_[4],name_RunningChickens,date_Jan,24,2079,venue,"sigma school",MedalType,RunningMedal
+print(group2)
+print(type(group2))
+group3=size_[6],name_SigmaShooters,date_Jan,26,2079,venue,("croissant school"),MedalType,SoccerMedal
+print(group3)
+print(type(group3))
+group4=size_[5],name_Gimme3,date_Jan,23,2079,venue,("spooder school"),MedalType,BasketballMedal
+print(group4)
+print(type(group4))
+group5=(size_[7],name_BatterUp,date_Jan,27,2079,venue,("dino school"),MedalType,BaseballMedal)
+print(group5)
+print(type(group5))"""
